c8b044bd458615f88b8f00c8ae2633880ac90edc
Elena Saburova <ext_elenas@playtika.com>
2019-10-22 17:20:45 +0300

    Fix dictionaries

--- Bot/Models/SpellCheckModel.cs
+++ Bot/Models/SpellCheckModel.cs
@@ -1,20 +1,22 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using WeCantSpell.Hunspell;
 
 namespace Bot.Models
 {
 	public class SpellCheckModel
 	{
-		private IWebHostEnvironment _appEnvironment;
+		private readonly IWebHostEnvironment _appEnvironment;
+		private readonly WordList _dictionary;
 
 		public SpellCheckModel(IWebHostEnvironment appEnvironment)
 		{
 			_appEnvironment = appEnvironment;
+			_dictionary = WordList.CreateFromFiles(_appEnvironment.WebRootPath + "/ru_RU.dic");
 		}
 		public string SpellCheck(string message)
 		{
-			var dictionary = WordList.CreateFromFiles(_appEnvironment.WebRootPath + "/ru_RU.dic");
 			var words = message.Split(' ', '\n');
 			var corrected = new List<string>();
 			string fullResponse = null;
@@ -29,10 +31,10 @@ namespace Bot.Models
 				});
 				if (!string.IsNullOrEmpty(cleanWord))
 				{
-					bool isCorrect = dictionary.Check(cleanWord);
+					bool isCorrect = _dictionary.Check(cleanWord);
 					if (!isCorrect)
 					{
-						var recommended = dictionary.Suggest(cleanWord);
+						var recommended = _dictionary.Suggest(cleanWord);
 						var checkerMessage = $"Неправильно: {cleanWord}. Варианты правильного написания: ";
 						foreach (var recommendation in recommended)
 						{
